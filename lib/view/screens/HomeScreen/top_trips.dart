24bc2e53d3256556ba47642ca21a7c92c00f57d0
Subhojeet Sahoo <Alphaisgod1@gmail.com>
2022-05-10 01:14:16 +0530

    container to display top trips

--- lib/view/screens/HomeScreen/top_trips.dart
+++ lib/view/screens/HomeScreen/top_trips.dart
@@ -5,22 +5,91 @@ import 'package:safari/controller/providers/places_provider.dart';
 import 'package:safari/model/place_model.dart';
 
 class TopTrips extends StatelessWidget {
-  const TopTrips({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // final List<PlaceModel> places =
-    //     Provider.of<PlacesProvider>(context).getBeaches;
-    return Container(
-      height: displayHeight(context) * 0.35,
-      width: displayWidth(context),
-      //color: Colors.black45,
-      child: Center(
-        child: Image.asset(
-          'assets/icons/explore.gif',
-          fit: BoxFit.cover,
+  viewPlaceTemplate(BuildContext context, PlaceModel place) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: const BorderSide(color: Colors.grey, width: 0.05)),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        width: displayWidth(context) * 0.5,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  place.images.first.toString(),
+                  height: displayHeight(context) * 0.24,
+                  width: displayWidth(context) * 0.5,
+                  fit: BoxFit.cover,
+                )),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: displayWidth(context) * 0.36,
+              child: Text(place.title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      letterSpacing: 0.1,
+                      color: Colors.teal[700],
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600)),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.location_pin,
+                  color: Colors.black54,
+                  size: 18,
+                ),
+                const VerticalDivider(
+                  width: 5,
+                ),
+                SizedBox(
+                  width: displayWidth(context) * 0.36,
+                  child: Text(place.location,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          color: Colors.black54,
+                          fontSize: 13.5,
+                          fontWeight: FontWeight.w500)),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<PlaceModel> places =
+        Provider.of<PlacesProvider>(context).getTopTrips;
+    return SizedBox(
+        height: displayHeight(context) * 0.35,
+        width: displayWidth(context),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return viewPlaceTemplate(context, places[index]);
+          },
+          itemCount: places.length,
+        ));
   }
 }
