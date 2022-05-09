6673360e86b64bd5921f17ea94c1809c682cd2c5
Subhojeet Sahoo <Alphaisgod1@gmail.com>
2022-05-10 01:13:17 +0530

    checking internet connectivity before sending get request

--- lib/controller/api_services/get_service.dart
+++ lib/controller/api_services/get_service.dart
@@ -1,20 +1,35 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 Future<dynamic> service(String api) async {
   try {
-    var response = await http.get(Uri.parse(api));
-    int statusCode = response.statusCode;
-    switch (statusCode) {
-      case 200:
-        return json.decode(response.body) as Map<String, dynamic>;
-      case 400:
-        return 'Bad request';
-      case 401:
-        return 'Unauthorised';
-      default:
-        break;
+    final internetResponse = await InternetAddress.lookup('google.com');
+    if (internetResponse.isEmpty) {
+      return;
     }
+    //print(internetResponse);
+    if (internetResponse.isNotEmpty &&
+        internetResponse[0].rawAddress.isNotEmpty) {
+      var response = await http.get(Uri.parse(api));
+      int statusCode = response.statusCode;
+      //print(statusCode);
+      switch (statusCode) {
+        case 200:
+          return json.decode(response.body) as Map<String, dynamic>;
+        case 400:
+          return 'Bad request';
+        case 401:
+          return 'Unauthorised';
+        default:
+          break;
+      }
+    } else {
+      return 'no internet';
+    }
+  } on SocketException catch (_) {
+    //print('2. no internet');
+    return 'no internet';
   } catch (e) {
     print(e.toString());
   }
