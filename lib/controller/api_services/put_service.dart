aa6f844529951cab567898a290eb01c86cdc8090
Subhojeet Sahoo <Alphaisgod1@gmail.com>
2022-05-12 01:14:37 +0530

    Api services modified

--- lib/controller/api_services/put_service.dart
+++ lib/controller/api_services/put_service.dart
@@ -3,18 +3,18 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:safari/controller/constants/service_api_constants.dart';
 
-class GetService {
-  Future<dynamic> service({required String endpoint}) async {
+class PutService {
+  Future<dynamic> service(
+      {required String endpoint, required Map<String, dynamic> body}) async {
     try {
       final internetResponse = await InternetAddress.lookup('google.com');
       if (internetResponse.isEmpty) {
         return;
       }
-      //print(internetResponse);
       if (internetResponse.isNotEmpty &&
           internetResponse[0].rawAddress.isNotEmpty) {
-        var response =
-            await http.get(Uri.parse(ServiceApi.base_url + endpoint));
+        var response = await http.put(Uri.parse(ServiceApi.base_url + endpoint),
+            body: json.encode(body));
         int statusCode = response.statusCode;
         //print(statusCode);
         switch (statusCode) {
@@ -31,10 +31,7 @@ class GetService {
         return 'no internet';
       }
     } on SocketException catch (_) {
-      //print('2. no internet');
       return 'no internet';
-    } catch (e) {
-      print(e.toString());
-    }
+    } catch (e) {}
   }
 }
